a88b72e37f772a5380a1fddd79521ac65b8567f4
“Sanket <sanketmetkar93@gmail.com>
2024-04-06 12:46:18 +0530

    Add project files.

---
+++ WebAPI/Home.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI
+{
+    public class Home
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? Address { get; set; }
+    }
+}
